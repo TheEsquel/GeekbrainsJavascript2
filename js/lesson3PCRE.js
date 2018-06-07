65329f589275fd757def3d97be09ba46f70b79a2
TheEsquel <rem.asadullin@gmail.com>
2018-06-07 19:09:16 +0500

    Finished  RegExp project

--- js/lesson3PCRE.js
+++ js/lesson3PCRE.js
@@ -7,16 +7,29 @@ class Form {
         this.success = true;
     }
     validateForm(){
-        let inputValue = this.input.value;
-        console.log(inputValue);
+    // считываем значение из переданного input     
+        let inputValue = this.input.val();
+        if(!inputValue.match(this.template)){
+            console.log('не совпадает ');
+            $('#nameValid').css('border', '2px red');
+            $('#validResult').text('Invalid!');
+            $('#validResult').css('color', 'red');
+        }else{
+            console.log('совпадает');
+            $('#nameValid').css('border', 'green');
+            $('#validResult').text('Valid!');
+            $('#validResult').css('color', 'green');
+        }
     }
 
 }
 
 $(function(){
-    $('#btnEdit').on('click', function(){
+    //  кнопка для валидации
+    $('#btnValid').on('click', function(){
+        // подгружаем   input 
         let nameInput = $('#nameValid'),
-            nameTemplate = /^[a-zA-Zа-яА-Я]+$/;
+            nameTemplate = /^[A-Za-zА-Яа-я]+$/;;
             
         let emailInput = $('#emailValid'),
             emailTemplate = /^[a-z]+(\.|-)?[a-z]+@[a-z]+\.[a-z]{2,4}$/;
@@ -24,10 +37,23 @@ $(function(){
         let phoneInput = $('#phoneValid'),
             phoneTemplate = /^\+\d\(\d{3}\)\d{3}\-\d{4}$/;
 
+    // создаем экземпляры класса Form через конструктор         
         let nameForm = new Form(nameInput, nameTemplate),
             emailForm = new Form(emailInput, emailTemplate),
             phoneForm = new Form(phoneInput, phoneTemplate);
+     
+        nameForm.validateForm();
+        emailForm.validateForm();
+        phoneForm.validateForm();
+    });
 
+    $('#btnEdit').on('click', function(){
+        let textValue = $('#textToEdit').val();
 
+        let textEdited = textValue.replace(/'/g, `"`);
+        console.log(textValue);
+        console.log(textEdited);
+        $('#editedText').text(textEdited);
+        $('#editedText').css({'color':'darkblue','font-size':'1.2em'});
     })
 })
